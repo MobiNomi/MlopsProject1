668e4ed4a76f453475fa5f3a779241625eff949a
Mubashir <mubashir5001177@gmail.com>
2026-08-07 02:26:31 -0700

    Fix repo_owner to point to correct DagsHub account

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -22,7 +22,7 @@ os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
-repo_owner = "vikashdas770"
+repo_owner = "MobiNomi"
 repo_name = "MlopsProject1"
 # Set up MLflow tracking URI
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
